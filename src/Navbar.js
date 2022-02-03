04817509b25d2f5f02394a5096241cf62a426e44
awooooool <45804108+awooooool@users.noreply.github.com>
2022-02-03 09:48:00 +0700

    Separate state for each modal

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,7 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
-function MyVerticallyCenteredModal(props) {
+function MyLoginModal(props) {
 
   return (
     <Modal
@@ -19,7 +19,31 @@ function MyVerticallyCenteredModal(props) {
       centered
     >
       <Modal.Body>
-        <h4>Centered Modal</h4>
+        <h4>Ini login</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+function MyRegisterModal(props) {
+
+  return (
+    <Modal
+      {...props}
+      size="md"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Body>
+        <h4>Ini Register</h4>
         <p>
           Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
           dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
@@ -35,33 +59,39 @@ function MyVerticallyCenteredModal(props) {
 
 
 function NavigationBar() {
-  const [modalShow, setModalShow] = React.useState(false);
-  return(
+  const [loginShow, setLoginShow] = React.useState(false);
+  const [registerShow, setRegisterShow] = React.useState(false);
+  return (
     <Navbar>
       <Container>
         <Navbar.Brand href="#home">
-        <img
-          src={img}
-          width="80"
-          height="80"
-          className="d-inline-block align-top"
-          alt="Logo WaysBucks"
-        />
+          <img
+            src={img}
+            width="80"
+            height="80"
+            className="d-inline-block align-top"
+            alt="Logo WaysBucks"
+          />
         </Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
           <Stack direction="horizontal" gap={3}>
-            <Button href="#login" value="login" className='px-5' variant="outline-danger" onClick={()=> setModalShow(true)}>Login</Button>{' '}
-            <Button href="#register" value="register" className='px-5' variant="danger" onClick={()=> setModalShow(true)}>Register</Button>{' '}
+            <Button href="#login" value="login" className='px-5' variant="outline-danger" onClick={() => setLoginShow(true)}>Login</Button>{' '}
+            <Button href="#register" value="register" className='px-5' variant="danger" onClick={() => setRegisterShow(true)}>Register</Button>{' '}
           </Stack>
         </Navbar.Collapse>
       </Container>
 
-      <MyVerticallyCenteredModal
-          show={modalShow}
-          onHide={() => setModalShow(false)}
+      <MyLoginModal
+        show={loginShow}
+        onHide={() => setLoginShow(false)}
       />
-      </Navbar>
+
+      <MyRegisterModal
+        show={registerShow}
+        onHide={() => setRegisterShow(false)}
+      />
+    </Navbar>
   )
 }
 
